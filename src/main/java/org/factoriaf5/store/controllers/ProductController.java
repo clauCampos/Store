fdfad4396980afa77eb66fcb149fe3c86780d820
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-05-17 12:02:54 +0200

    prod. controller

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -1,2 +1,12 @@
-package org.factoriaf5.store.controllers;public class ProductController {
-}
+package org.factoriaf5.store.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @GetMapping("/health")
+    public String check() {
+        return "Hello I'm a server";
+    }
+};
